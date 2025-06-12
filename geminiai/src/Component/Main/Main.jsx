a47403117b42244c390829ead8b4ca5a624b9206
sagnikBag <146710246+Sagnikrick568@users.noreply.github.com>
2025-06-12 12:22:37 +0530

    sagnik

--- geminiai/src/Component/Main/Main.jsx
+++ geminiai/src/Component/Main/Main.jsx
@@ -6,8 +6,11 @@ import { URL } from '../../config/gemini'
 
 const Main = () => {
 
-const [question,setQuestion] = useState("")
-const [result,setResult] = useState(undefined)
+const [question,setQuestion] = useState(undefined)
+const [result,setResult] = useState("")
+const [loading,setLoading] = useState(false)
+const [recentPrompt,setRecentPrompt] = useState("")
+
 
 
 const payload ={
@@ -23,6 +26,8 @@ const payload ={
   }
 
 const askQuestion = async()=>{
+
+  
   let response = await fetch(URL,{
     method:"POST",
     body:JSON.stringify(payload)
@@ -30,8 +35,13 @@ const askQuestion = async()=>{
  response = await response.json();
 
 //  console.log(response.candidates[0].content.parts[0].text);
- setResult(response.candidates[0].content.parts[0].text);
-
+  
+  setRecentPrompt(question);
+  
+ 
+  setResult(response.candidates[0].content.parts[0].text);
+   setLoading(false);
+ 
  
 }
 
@@ -45,9 +55,10 @@ const askQuestion = async()=>{
       </div>
       <div className="main-container">
         <div>
-          {result}
-        </div>
-        <div className="greet">
+          {!result
+          ?
+          <>
+          <div className="greet">
           <p><span>Hello, Dev</span></p>
           <p>How can I help you today?</p>
         </div>
@@ -69,6 +80,52 @@ const askQuestion = async()=>{
             <img src={assets.code_icon} alt="" />
           </div>
         </div>
+          </>
+          :
+          <div className='result'>
+            <div className="result-title">
+              <img src={assets.user_icon} alt="" />
+              <p>{recentPrompt}</p>
+            </div>
+            <div className="result-data">
+              <img src={assets.gemini_icon} alt="" />
+              {loading
+              ?<div className='loader'>
+                <hr />
+                <hr />
+                <hr />
+              </div>
+              :
+              <p dangerouslySetInnerHTML={{__html:result}}></p>
+             
+              }
+              
+            </div>
+          </div>
+          }
+        </div>
+        {/* // <div className="greet">
+        //   <p><span>Hello, Dev</span></p>
+        //   <p>How can I help you today?</p>
+        // </div>
+        // <div className="cards">
+        //   <div className="card">
+        //     <p>Suggest beautiful places to see on an upcoming road trip</p>
+        //     <img src={assets.compass_icon} alt="" />
+        //   </div>
+        //   <div className="card">
+        //     <p>Briefly summarize this concept: urban planning</p>
+        //     <img src={assets.bulb_icon} alt="" />
+        //   </div>
+        //   <div className="card">
+        //     <p>Brainstorm team bonding activitise for our work retreat</p>
+        //     <img src={assets.message_icon} alt="" />
+        //   </div>
+        //   <div className="card">
+        //     <p>Improve the readability of the following</p>
+        //     <img src={assets.code_icon} alt="" />
+        //   </div>
+        // </div> */}
         <div className="main-bottom">
           <div className="search-box">
             <input type="text" value={question} onChange={(event)=>setQuestion(event.target.value)} placeholder='Enter promt here' />
